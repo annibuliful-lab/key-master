684a714fa3b13fce209a1d5d7299b0134d115a52
lagmanzaza <lagmanmc@gmail.com>
2022-05-07 14:06:10 +0700

    feat: cleanup db function

--- prisma/seed/index.ts
+++ prisma/seed/index.ts
@@ -6,8 +6,34 @@ import { createPermissions } from './permission';
 import { createProject } from './project/project';
 import { createProjectRoleAndPermissions } from './project/project-role';
 import { createUser } from './user';
+import { prismaClient } from '../client';
+
+export async function cleanupDb() {
+  const shouldClearTable = true;
+
+  if (shouldClearTable) {
+    const tablenames = await prismaClient.$queryRaw<
+      Array<{ tablename: string }>
+    >`SELECT tablename FROM pg_tables WHERE schemaname='public'`;
+
+    for (const { tablename } of tablenames) {
+      if (tablename !== '_prisma_migrations') {
+        try {
+          const result = await prismaClient.$executeRawUnsafe(
+            `TRUNCATE TABLE "public"."${tablename}" CASCADE;`
+          );
+          console.log(`cleanup table = ${tablename}`, result);
+        } catch (error) {
+          console.log('clean up error => ', error);
+        }
+      }
+    }
+  }
+}
 
 const main = async () => {
+  await cleanupDb();
+
   await createPermissions();
   await createUser();
   await createProject();
