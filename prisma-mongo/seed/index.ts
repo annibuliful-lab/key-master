88e586fa6a8c2bab8e2271967768666cacb71a65
lagmanzaza <lagmanmc@gmail.com>
2022-05-07 21:44:26 +0700

    feat: mongodb seed

--- prisma-mongo/seed/index.ts
+++ prisma-mongo/seed/index.ts
@@ -1,8 +1,19 @@
 import { mongoClient } from '../client';
 async function main() {
+  const audit = await mongoClient.auditLog.create({
+    data: {
+      projectId: 'TEST_PROJECT_ID',
+      serviceName: 'TEST_SERVICE_NAME',
+      type: 'CREATE',
+      status: 'INFO',
+      createdBy: 'TEST_USER',
+      updatedBy: 'TEST_USER',
+    },
+  });
   const createMessage = await mongoClient.message.create({
     data: {
       content: 'TEST_DATA',
+      auditId: audit.id,
     },
   });
   console.log('created Message', { createMessage });
