3bf6dbac3fbbaf70154f5e15633e758c73220ae7
lagmanzaza <lagmanmc@gmail.com>
2022-05-08 09:02:12 +0700

    feat: project role user schema

--- apps/project-backend/src/context.ts
+++ apps/project-backend/src/context.ts
@@ -4,12 +4,14 @@ import { ProjectService } from './services/project.service';
 import { projectRoleDataLoader } from './dataloaders/project-role.dataloader';
 import { ProjectOrganizationService } from './services/project-organization.service';
 import { projectDataLoader } from './dataloaders/project.dataloader';
+import { ProjectRoleUserService } from './services/project-role-user.service';
 
 export interface IGraphqlContext {
   project: ProjectService;
   projectRole: ProjectRoleService;
   projectRolePermission: ProjectRolePermissionService;
   projectOrganization: ProjectOrganizationService;
+  projectRoleUser: ProjectRoleUserService;
   projectRoleDataLoader: typeof projectRoleDataLoader;
   projectDataLoader: typeof projectDataLoader;
 }
