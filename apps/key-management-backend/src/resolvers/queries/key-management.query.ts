50224c6fef77d3cd64f51eb2e0a284b9900e2c39
lagmanzaza <lagmanmc@gmail.com>
2022-05-08 17:55:14 +0700

    fix: null field error

--- apps/key-management-backend/src/resolvers/queries/key-management.query.ts
+++ apps/key-management-backend/src/resolvers/queries/key-management.query.ts
@@ -2,10 +2,10 @@ import { Resolvers } from '../../codegen-generated';
 import { IGraphqlContext } from '../../context';
 
 export const queries: Resolvers<IGraphqlContext>['Query'] = {
-  getOrganizationKeyManagementById: (_parent, { id }, ctx) => {
-    return ctx.organizationKeyManagement.findById(id);
+  getKeyManagementById: (_parent, { id }, ctx) => {
+    return ctx.keyManagement.findById(id);
   },
-  getOrganizationKeyManagements: (_parent, { filter }, ctx) => {
-    return ctx.organizationKeyManagement.findManyByFilter(filter);
+  getKeyManagementByIds: (_parent, { ids }, ctx) => {
+    return ctx.keyManagement.findByIds(ids);
   },
 };
--- apps/key-management-backend/src/resolvers/queries/organization-key-management.query.ts
+++ apps/key-management-backend/src/resolvers/queries/organization-key-management.query.ts
@@ -5,4 +5,7 @@ export const queries: Resolvers<IGraphqlContext>['Query'] = {
   getOrganizationKeyManagementById: (_parent, { id }, ctx) => {
     return ctx.organizationKeyManagement.findById(id);
   },
+  getOrganizationKeyManagements: (_parent, { filter }, ctx) => {
+    return ctx.organizationKeyManagement.findManyByFilter(filter);
+  },
 };
