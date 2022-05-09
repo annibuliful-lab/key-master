126295325031e3bc51d0c21cf2f1a3c55c51cdf8
lagmanzaza <lagmanmc@gmail.com>
2022-05-09 12:13:59 +0700

    fix: duplicated create prisma client

--- libs/db/src/mongo-client.ts
+++ libs/db/src/mongo-client.ts
@@ -1,43 +1,55 @@
 import { PrismaClient } from '@prisma/client-mongo';
-
 import { config } from 'dotenv';
 
 config();
 
 const shouldLog = process.env.LOG_PRISMA_CLIENT === 'true';
 
-export const mongoClient = new PrismaClient(
-  shouldLog
-    ? {
-        log: [
-          {
-            emit: 'event',
-            level: 'query',
-          },
-          {
-            emit: 'stdout',
-            level: 'error',
-          },
-          {
-            emit: 'stdout',
-            level: 'info',
-          },
-          {
-            emit: 'stdout',
-            level: 'warn',
-          },
-        ],
-      }
-    : undefined
-);
+let _prismaClient: PrismaClient = null;
 
-if (shouldLog) {
-  mongoClient.$on('query', (e) => {
-    console.log('\n');
-    console.log(`Target: ${e.target}`);
-    console.log(`Query: ${e.query}`);
-    console.log(`Param: ${e.params}`);
-    console.log(`Duration: ${e.duration} ms`);
-    console.log('\n');
-  });
+function getClient() {
+  if (_prismaClient) {
+    return _prismaClient;
+  }
+
+  _prismaClient = new PrismaClient(
+    shouldLog
+      ? {
+          log: [
+            {
+              emit: 'event',
+              level: 'query',
+            },
+            {
+              emit: 'stdout',
+              level: 'error',
+            },
+            {
+              emit: 'stdout',
+              level: 'info',
+            },
+            {
+              emit: 'stdout',
+              level: 'warn',
+            },
+          ],
+        }
+      : undefined
+  );
+
+  if (shouldLog) {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (_prismaClient.$on as any)('query', (e) => {
+      console.log('\n');
+      console.log(`Target: ${e.target}`);
+      console.log(`Query: ${e.query}`);
+      console.log(`Param: ${e.params}`);
+      console.log(`Duration: ${e.duration} ms`);
+      console.log('\n');
+    });
+  }
+
+  return _prismaClient;
 }
+
+export const mongoClient = getClient();
--- libs/db/src/prisma-client.ts
+++ libs/db/src/prisma-client.ts
@@ -5,38 +5,51 @@ config();
 
 const shouldLog = process.env.LOG_PRISMA_CLIENT === 'true';
 
-export const prismaClient = new PrismaClient(
-  shouldLog
-    ? {
-        log: [
-          {
-            emit: 'event',
-            level: 'query',
-          },
-          {
-            emit: 'stdout',
-            level: 'error',
-          },
-          {
-            emit: 'stdout',
-            level: 'info',
-          },
-          {
-            emit: 'stdout',
-            level: 'warn',
-          },
-        ],
-      }
-    : undefined
-);
+let _prismaClient: PrismaClient = null;
 
-if (shouldLog) {
-  prismaClient.$on('query', (e) => {
-    console.log('\n');
-    console.log(`Target: ${e.target}`);
-    console.log(`Query: ${e.query}`);
-    console.log(`Param: ${e.params}`);
-    console.log(`Duration: ${e.duration} ms`);
-    console.log('\n');
-  });
+function getClient() {
+  if (_prismaClient) {
+    return _prismaClient;
+  }
+
+  _prismaClient = new PrismaClient(
+    shouldLog
+      ? {
+          log: [
+            {
+              emit: 'event',
+              level: 'query',
+            },
+            {
+              emit: 'stdout',
+              level: 'error',
+            },
+            {
+              emit: 'stdout',
+              level: 'info',
+            },
+            {
+              emit: 'stdout',
+              level: 'warn',
+            },
+          ],
+        }
+      : undefined
+  );
+
+  if (shouldLog) {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (_prismaClient.$on as any)('query', (e) => {
+      console.log('\n');
+      console.log(`Target: ${e.target}`);
+      console.log(`Query: ${e.query}`);
+      console.log(`Param: ${e.params}`);
+      console.log(`Duration: ${e.duration} ms`);
+      console.log('\n');
+    });
+  }
+
+  return _prismaClient;
 }
+
+export const prismaClient = getClient();
