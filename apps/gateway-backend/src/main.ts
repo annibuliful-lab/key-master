8ae6c6d0a5679487dffea0d5a7f9938959a39087
lagmanzaza <lagmanmc@gmail.com>
2022-05-11 21:18:18 +0700

    chore: remove unused

--- apps/gateway-backend/src/main.ts
+++ apps/gateway-backend/src/main.ts
@@ -73,57 +73,56 @@ interface IGatewayContext {
   authorization: string;
 }
 
-const validateWsAuthentication =
-  (shouldSendForbiddenCode: boolean) => async (headers: IGatewayContext) => {
-    const authorization = headers['authorization'];
-    const allowTestSecret = headers['x-allow-test'] as string;
-    const projectId = headers['x-project-id'] as string;
-    const userId = headers['x-user-id'] as string;
-    const permissions = (headers['x-user-permissions'] ?? '') as string;
-    const token = authorization?.replace('Bearer ', '');
+const validateWsAuthentication = async (headers: IGatewayContext) => {
+  const authorization = headers['authorization'];
+  const allowTestSecret = headers['x-allow-test'] as string;
+  const projectId = headers['x-project-id'] as string;
+  const userId = headers['x-user-id'] as string;
+  const permissions = (headers['x-user-permissions'] ?? '') as string;
+  const token = authorization?.replace('Bearer ', '');
 
-    if (!token) {
-      throw new AuthenticationError('Unauthorization');
-    }
+  if (!token) {
+    throw new AuthenticationError('Unauthorization');
+  }
 
-    if (
-      authorization?.startsWith('TEST-AUTH') &&
-      allowTestSecret === process.env.SKIP_AUTH_SECRET
-    ) {
-      return {
-        'x-user-id': userId,
-        'x-project-id': projectId,
-        'x-user-permissions': !isEmpty(permissions)
-          ? permissions.split(',')
-          : (
-              await getUserPermissions({
-                projectId,
-                userId,
-              })
-            ).permissions,
-        'x-user-role': 'KeyAdmin',
-        authorization,
-      };
-    }
-
-    const userAuth = await validateAuthentication({ token, projectId });
-
-    if (typeof userAuth === 'string' && userAuth === 'FORBIDDEN') {
-      throw new ForbiddenError(`Forbidden with project id ${projectId}`);
-    }
-
-    if (!userAuth) {
-      throw new AuthenticationError('Unauthorization');
-    }
-
+  if (
+    authorization?.startsWith('TEST-AUTH') &&
+    allowTestSecret === process.env.SKIP_AUTH_SECRET
+  ) {
     return {
-      'x-user-id': userAuth.userId,
+      'x-user-id': userId,
       'x-project-id': projectId,
-      'x-user-permissions': userAuth.permissions,
-      'x-user-role': userAuth.role,
+      'x-user-permissions': !isEmpty(permissions)
+        ? permissions.split(',')
+        : (
+            await getUserPermissions({
+              projectId,
+              userId,
+            })
+          ).permissions,
+      'x-user-role': 'KeyAdmin',
       authorization,
     };
+  }
+
+  const userAuth = await validateAuthentication({ token, projectId });
+
+  if (typeof userAuth === 'string' && userAuth === 'FORBIDDEN') {
+    throw new ForbiddenError(`Forbidden with project id ${projectId}`);
+  }
+
+  if (!userAuth) {
+    throw new AuthenticationError('Unauthorization');
+  }
+
+  return {
+    'x-user-id': userAuth.userId,
+    'x-project-id': projectId,
+    'x-user-permissions': userAuth.permissions,
+    'x-user-role': userAuth.role,
+    authorization,
   };
+};
 const main = async () => {
   const enablePlayGround =
     process.env.ENABLE_GRAPHQL_SERVER_PLAYGROUND === 'true';
@@ -145,11 +144,11 @@ const main = async () => {
         ctx
       ): Promise<Record<string, unknown> | boolean | void> => {
         const headers = ctx.connectionParams as unknown as IGatewayContext;
-        return await validateWsAuthentication(false)(headers);
+        return await validateWsAuthentication(headers);
       },
       onSubscribe: async (ctx) => {
         const headers = ctx.connectionParams as unknown as IGatewayContext;
-        await validateWsAuthentication(true)(headers);
+        await validateWsAuthentication(headers);
       },
     },
     wsServer
