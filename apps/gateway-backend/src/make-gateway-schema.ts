0fa617e2d38a8bd2835d1cf8b22775e88a7d875f
lagmanzaza <lagmanmc@gmail.com>
2022-05-11 21:36:09 +0700

    refactor: move remote schema

---
+++ apps/gateway-backend/src/make-gateway-schema.ts
@@ -0,0 +1,47 @@
+import { stitchSchemas } from '@graphql-tools/stitch';
+import { stitchingDirectives } from '@graphql-tools/stitching-directives';
+import { executeRemoteSchema } from '@key-master/graphql';
+
+const { stitchingDirectivesTransformer } = stitchingDirectives();
+
+export const makeGatewaySchema = async () => {
+  const userSchema = await executeRemoteSchema({
+    host: 'localhost',
+    port: 3000,
+    supportWs: true,
+  });
+
+  const authSchema = await executeRemoteSchema({
+    host: 'localhost',
+    port: 3001,
+    supportWs: true,
+  });
+
+  const projectSchema = await executeRemoteSchema({
+    host: 'localhost',
+    port: 3002,
+  });
+
+  const keyManagementSchema = await executeRemoteSchema({
+    host: 'localhost',
+    port: 3003,
+  });
+
+  return stitchSchemas({
+    subschemaConfigTransforms: [stitchingDirectivesTransformer],
+    subschemas: [
+      {
+        schema: userSchema,
+      },
+      {
+        schema: authSchema,
+      },
+      {
+        schema: projectSchema,
+      },
+      {
+        schema: keyManagementSchema,
+      },
+    ],
+  });
+};
