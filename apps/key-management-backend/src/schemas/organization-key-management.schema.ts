81b52aacca1c321f74e3acb3a38e53df0ce87611
lagmanzaza <lagmanmc@gmail.com>
2022-05-11 22:25:39 +0700

    feat: org key management access directive

--- apps/key-management-backend/src/schemas/organization-key-management.schema.ts
+++ apps/key-management-backend/src/schemas/organization-key-management.schema.ts
@@ -33,23 +33,28 @@ export const typeDefs = gql`
     createOrganizationKeyManagement(
       input: CreateOrganizationKeyManagementInput!
     ): OrganizationKeyManagement!
+      @access(conditions: { permission: "ORGANIZATION_KEY_MANAGEMENT_WRITE" })
 
     updateOrganizationKeyManagement(
       id: ID!
       input: UpdateOrganizationKeyManagementInput!
     ): OrganizationKeyManagement!
+      @access(conditions: { permission: "ORGANIZATION_KEY_MANAGEMENT_WRITE" })
 
     deleteOrganizationKeyManagement(
       id: ID!
       pin: String!
     ): DeleteOrganizationKeyManagementOperationResult!
+      @access(conditions: { permission: "ORGANIZATION_KEY_MANAGEMENT_WRITE" })
   }
 
   type Query {
     getOrganizationKeyManagementById(id: ID!): OrganizationKeyManagement!
+      @access(conditions: { permission: "ORGANIZATION_KEY_MANAGEMENT_READ" })
 
     getOrganizationKeyManagements(
       filter: OrganizationKeyManagementFilterInput!
     ): [OrganizationKeyManagement!]!
+      @access(conditions: { permission: "ORGANIZATION_KEY_MANAGEMENT_READ" })
   }
 `;
