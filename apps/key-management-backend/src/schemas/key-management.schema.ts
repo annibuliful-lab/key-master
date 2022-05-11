a2f94bf4ce25ca3ecdddfc28af76b0b88e42d463
lagmanzaza <lagmanmc@gmail.com>
2022-05-11 22:24:55 +0700

    feat: key management permission with directive

--- apps/key-management-backend/src/schemas/key-management.schema.ts
+++ apps/key-management-backend/src/schemas/key-management.schema.ts
@@ -30,25 +30,32 @@ export const typeDefs = gql`
 
   type Mutation {
     createKeyManagement(input: CreateKeyManagementInput!): KeyManagement!
+      @access(conditions: { permission: "KEY_MANAGEMENT_WRITE" })
 
     updateKeyManagement(
       id: ID!
       input: UpdateKeyManagementInput!
     ): KeyManagement!
+      @access(conditions: { permission: "KEY_MANAGEMENT_WRITE" })
 
     updateKeyManagementPin(
       id: ID!
       input: UpdateKeyManagementPinInput!
     ): KeyManagement!
+      @access(conditions: { permission: "KEY_MANAGEMENT_UPDATE_PIN" })
 
     deleteKeyMangement(
       id: ID!
       pin: String!
     ): KeyManagementDeleteOperationResult!
+      @access(conditions: { permission: "KEY_MANAGEMENT_WRITE" })
   }
 
   type Query {
     getKeyManagementById(id: ID!): KeyManagement!
+      @access(conditions: { permission: "KEY_MANAGEMENT_READ" })
+
     getKeyManagementByIds(ids: [ID!]!): [KeyManagement!]!
+      @access(conditions: { permission: "KEY_MANAGEMENT_READ" })
   }
 `;
