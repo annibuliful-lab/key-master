6743f668fe174c57467a2d8ae869d23c5ed813a4
lagmanzaza <lagmanmc@gmail.com>
2022-05-14 14:36:13 +0700

    chore: coding style

--- apps/project-backend/src/resolvers/mutations/organization-user-key-bookmark.mutation.ts
+++ apps/project-backend/src/resolvers/mutations/organization-user-key-bookmark.mutation.ts
@@ -2,8 +2,10 @@ import { Resolvers } from '../../codegen-generated';
 import { IGraphqlContext } from '../../context';
 
 export const mutations: Resolvers<IGraphqlContext>['Mutation'] = {
-  createOrganizationKeyManagementUserBookmark: (_parent, { input }, ctx) =>
-    ctx.organizationUserKeyBookmark.create(input),
-  deletedOrganizationKeyManagementUserBookmark: (_parent, { id }, ctx) =>
-    ctx.organizationUserKeyBookmark.delete(id),
+  createOrganizationKeyManagementUserBookmark: (_parent, { input }, ctx) => {
+    return ctx.organizationUserKeyBookmark.create(input);
+  },
+  deletedOrganizationKeyManagementUserBookmark: (_parent, { id }, ctx) => {
+    return ctx.organizationUserKeyBookmark.delete(id);
+  },
 };
--- apps/project-backend/src/resolvers/queries/organization-user-key-bookmark.query.ts
+++ apps/project-backend/src/resolvers/queries/organization-user-key-bookmark.query.ts
@@ -2,8 +2,10 @@ import { Resolvers } from '../../codegen-generated';
 import { IGraphqlContext } from '../../context';
 
 export const queries: Resolvers<IGraphqlContext>['Query'] = {
-  getOrganizationKeyManagementUserBookmarkById: (_parent, { id }, ctx) =>
-    ctx.organizationUserKeyBookmark.findById(id),
-  getOrganizationKeyManagementUserBookmarks: (_parent, { filter }, ctx) =>
-    ctx.organizationUserKeyBookmark.findManyByFilter(filter),
+  getOrganizationKeyManagementUserBookmarkById: (_parent, { id }, ctx) => {
+    return ctx.organizationUserKeyBookmark.findById(id);
+  },
+  getOrganizationKeyManagementUserBookmarks: (_parent, { filter }, ctx) => {
+    return ctx.organizationUserKeyBookmark.findManyByFilter(filter);
+  },
 };
