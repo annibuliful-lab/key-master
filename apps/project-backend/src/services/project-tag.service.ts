bd21d1424678eff784910a5590a999b6e4ce2561
lagmanzaza <lagmanmc@gmail.com>
2022-05-15 10:24:39 +0700

    feat: delete project tag

--- apps/project-backend/src/services/project-tag.service.ts
+++ apps/project-backend/src/services/project-tag.service.ts
@@ -4,6 +4,7 @@ import {
   IAppContext,
   ResourceNotFound,
 } from '@key-master/graphql';
+import { ForbiddenError } from 'apollo-server-fastify';
 import {
   CreateProjectTagInput,
   UpdateProjectTagInput,
@@ -39,7 +40,7 @@ export class ProjectTagService extends Repository<IAppContext> {
   async update(id: string, input: UpdateProjectTagInput) {
     const projectTag = await this.db.projectTag.findFirst({
       select: {
-        id: true,
+        projectId: true,
       },
       where: {
         id,
@@ -51,12 +52,43 @@ export class ProjectTagService extends Repository<IAppContext> {
       throw new ResourceNotFound(`update project tag: ${id} not found`);
     }
 
+    if (projectTag.projectId !== this.context.projectId) {
+      throw new ForbiddenError('you are not in this project');
+    }
+
     return this.db.projectTag.update({
       where: {
         id,
       },
       data: {
         tag: input.tag,
+        updatedBy: this.context.userId,
+      },
+    });
+  }
+
+  async delete(id: string) {
+    const projectTag = await this.db.projectTag.findFirst({
+      select: { projectId: true },
+      where: {
+        id,
+        deletedAt: null,
+      },
+    });
+    if (!projectTag) {
+      throw new ResourceNotFound(`delete project tag: ${id} not found`);
+    }
+
+    if (projectTag.projectId !== this.context.projectId) {
+      throw new ForbiddenError('you are not in this project');
+    }
+
+    return this.db.projectTag.update({
+      where: {
+        id,
+      },
+      data: {
+        deletedAt: new Date(),
         updatedBy: this.context.userId,
       },
     });
