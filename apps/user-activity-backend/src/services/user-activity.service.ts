ae69659a7f5efe987583ad4c772376ad65119b3e
lagmanzaza <lagmanmc@gmail.com>
2022-05-15 20:38:17 +0700

    feat: create by id and find by id with pagination

--- apps/user-activity-backend/src/services/user-activity.service.ts
+++ apps/user-activity-backend/src/services/user-activity.service.ts
@@ -1,13 +1,48 @@
 import { MongoRepository } from '@key-master/db';
 import { IAppContext } from '@key-master/graphql';
-import { UserActivityFilterInput } from '../codegen-generated';
+import {
+  CreateUserActivityInput,
+  KeyManagementHistoryLogFilterInput,
+  UserActivityFilterInput,
+} from '../codegen-generated';
 
 export class UserActivityService extends MongoRepository<IAppContext> {
+  create(data: CreateUserActivityInput) {
+    return this.db.userActivity.create({
+      data: {
+        parentPkId: data.parentPkId,
+        userId: data.userId,
+        description: data.description,
+        serviceName: data.serviceName,
+        type: data.type,
+        data: data.data,
+      },
+    });
+  }
+
   findById(id: string) {
     return this.db.userActivity.findUnique({
       where: {
         id,
       },
+    });
+  }
+
+  findByIdWithPagination(
+    parentPkId: string,
+    serviceName: string,
+    filter: KeyManagementHistoryLogFilterInput
+  ) {
+    return this.db.userActivity.findMany({
+      where: {
+        parentPkId,
+        serviceName,
+      },
+      orderBy: {
+        createdAt: 'asc',
+      },
+      skip: filter?.skip ?? 0,
+      take: filter?.take ?? 20,
     });
   }
 
@@ -20,6 +55,9 @@ export class UserActivityService extends MongoRepository<IAppContext> {
         },
       }),
       where: {
+        ...(filter?.parentPkId && {
+          parentPkId: filter?.parentPkId,
+        }),
         ...(filter?.search && {
           description: {
             contains: filter?.search,
