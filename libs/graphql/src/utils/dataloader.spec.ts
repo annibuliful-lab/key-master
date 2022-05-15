fc42474486c6ff37c367cdfb6c3da04dfbd00100
lagmanzaza <lagmanmc@gmail.com>
2022-05-15 11:02:13 +0700

    feat: dataloader utils for multiple objects

--- libs/graphql/src/utils/dataloader.spec.ts
+++ libs/graphql/src/utils/dataloader.spec.ts
@@ -1,4 +1,8 @@
-import { mapDataWithIds, mapDataWithIdsByCustomFieldId } from './dataloader';
+import {
+  mapDataWithIds,
+  mapDataWithIdsByCustomFieldId,
+  mapDatasWithIdsByCustomField,
+} from './dataloader';
 
 describe('dataloader', () => {
   describe('mapDataWithIds', () => {
@@ -20,6 +24,34 @@ describe('dataloader', () => {
           ],
           ids: ['1', '2'],
           idField: 'id',
+        })
+      ).toMatchSnapshot();
+    });
+  });
+
+  describe('mapDatasWithIdsByCustomField', () => {
+    it('returns map muliple data structure with custom field', () => {
+      expect(
+        mapDatasWithIdsByCustomField({
+          data: [
+            {
+              id: '2860e6ff-87ef-44c8-995f-7c4b516c1794',
+              projectId: 'TEST_PROJECT_ID_1',
+              tag: 'TAG_A',
+            },
+            {
+              id: '6f5b77dd-7859-4229-86d8-1b92063b24e1',
+              projectId: 'TEST_PROJECT_ID_2',
+              tag: 'TAG_B',
+            },
+            {
+              id: '2860e6ff-87ef-44c8-995f-7c4b516c1890',
+              projectId: 'TEST_PROJECT_ID_1',
+              tag: 'TAG_C',
+            },
+          ],
+          ids: ['TEST_PROJECT_ID_1', 'TEST_PROJECT_ID_2'],
+          idField: 'projectId',
         })
       ).toMatchSnapshot();
     });
