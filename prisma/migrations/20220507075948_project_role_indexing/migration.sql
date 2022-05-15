948877c42336db82742868b2846cd32ecada551e
lagmanzaza <lagmanmc@gmail.com>
2022-05-15 09:54:58 +0700

    feat: base line

--- prisma/migrations/20220507075948_project_role_indexing/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropIndex
-DROP INDEX "ProjectRole_projectId_role_key";
-
--- CreateIndex
-CREATE INDEX "ProjectRole_projectId_role_idx" ON "ProjectRole"("projectId", "role");
--- prisma/migrations/20220507234607_organization_indexing/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropIndex
-DROP INDEX "ProjectOrganization_projectId_name_key";
-
--- CreateIndex
-CREATE INDEX "ProjectOrganization_projectId_name_idx" ON "ProjectOrganization"("projectId", "name");
