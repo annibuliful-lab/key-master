4351b8e0b0a71ac17a5c9f4f522d975c6625f620
lagmanzaza <lagmanmc@gmail.com>
2022-05-15 17:32:11 +0700

    fix: change client to create and read permissions

--- libs/test/src/graphql-client-utils/permission.ts
+++ libs/test/src/graphql-client-utils/permission.ts
@@ -1,11 +1,11 @@
-import { projectOwnerAClient } from '../graphql-client';
+import { adminOwnerClient } from '../graphql-client';
 import { nanoid } from 'nanoid';
 
 export function createPermission(customPermission?: string) {
   const permission = customPermission
     ? customPermission
     : `MOCK_PERMISSION_${nanoid()}`;
-  return projectOwnerAClient.chain.mutation
+  return adminOwnerClient.chain.mutation
     .createPermission({ permission })
     .get({ permission: true, id: true });
 }
