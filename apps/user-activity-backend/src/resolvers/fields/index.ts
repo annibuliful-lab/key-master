1c3320b0fa46a9cbebbef58cd9b497a9297b781a
lagmanzaza <lagmanmc@gmail.com>
2022-05-15 20:22:39 +0700

    chore: import field resolver

---
+++ apps/user-activity-backend/src/resolvers/fields/index.ts
@@ -0,0 +1,5 @@
+import { Resolvers } from '../../codegen-generated';
+import { resolvers as KeyManagementActivityFieldsResolvers } from './key-management-activity.field';
+export const fieldResolvers: Resolvers = {
+  ...KeyManagementActivityFieldsResolvers,
+};
--- apps/user-activity-backend/src/resolvers/index.ts
+++ apps/user-activity-backend/src/resolvers/index.ts
@@ -1,6 +1,8 @@
 import { Resolvers } from '../codegen-generated';
+import { fieldResolvers } from './fields';
 import { queries } from './queries';
 
 export const resolvers: Resolvers = {
   Query: queries,
+  ...fieldResolvers,
 };
