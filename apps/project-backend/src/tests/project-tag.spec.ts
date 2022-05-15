22b33aa84a5e3f5b703616c148417317fe739734
lagmanzaza <lagmanmc@gmail.com>
2022-05-15 10:24:50 +0700

    test: update project tag

--- apps/project-backend/src/tests/project-tag.spec.ts
+++ apps/project-backend/src/tests/project-tag.spec.ts
@@ -2,6 +2,7 @@ import {
   Client,
   createProjectTag,
   expectDuplicatedError,
+  expectNotFoundError,
   projectOwnerAClient,
 } from '@key-master/test';
 import { nanoid } from 'nanoid';
@@ -47,6 +48,43 @@ describe('Project Tag', () => {
           })
           .get({
             projectId: true,
+            id: true,
+            tag: true,
+          })
+      );
+    });
+
+    it('updates by id', async () => {
+      const createdTag = await createProjectTag({
+        client,
+      });
+      const newTag = `MOCK_NEW_TAG_${nanoid()}`;
+      const updatedTag = await client.chain.mutation
+        .updateProjectTag({
+          id: createdTag.id,
+          input: {
+            tag: newTag,
+          },
+        })
+        .get({
+          id: true,
+          tag: true,
+        });
+
+      expect(updatedTag.tag).toEqual(newTag);
+      expect(updatedTag.id).toEqual(createdTag.id);
+    });
+
+    it('throws not found error when update with wrong id', () => {
+      expectNotFoundError(
+        client.chain.mutation
+          .updateProjectTag({
+            id: `MOCK_WRONG_PROJECT_TAG_ID_${nanoid()}`,
+            input: {
+              tag: `newTag`,
+            },
+          })
+          .get({
             id: true,
             tag: true,
           })
