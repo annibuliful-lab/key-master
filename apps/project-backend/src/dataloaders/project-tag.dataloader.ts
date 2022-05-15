5c329709544d0cb1361144a2172e5f38e551a509
lagmanzaza <lagmanmc@gmail.com>
2022-05-15 11:14:00 +0700

    chore: log

--- apps/project-backend/src/dataloaders/project-tag.dataloader.ts
+++ apps/project-backend/src/dataloaders/project-tag.dataloader.ts
@@ -6,7 +6,7 @@ export const projectTagDataLoader = new DataLoader(async (ids: string[]) => {
   const projectTags = await prismaClient.projectTag.findMany({
     where: { projectId: { in: [...new Set(ids)] } },
   });
-  console.log('tags', projectTags);
+
   return mapDatasWithIdsByCustomField({
     data: projectTags,
     idField: 'projectId',
